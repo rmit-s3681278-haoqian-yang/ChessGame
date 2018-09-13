c3b79131e32ed059f7b06dacd5639797e7d66a6b
rmit-s3681278-haoqian-yang <42133486+rmit-s3681278-haoqian-yang@users.noreply.github.com>
2018-09-13 19:58:03 +1000

    Made some changes
    jiji

--- src/Tables/Table.java
+++ src/Tables/Table.java
@@ -1,5 +1,30 @@
 package Tables;
 
+import java.util.HashMap;
+
+import Tables.Players.Player;
+
 public class Table {
 
+	
+	Board gameBoard = new Board();
+	HashMap<Player,String> PlayerLib = new HashMap<>();
+	
+	
+	
+	
+	public boolean AddPlayer(String name, String pass) {
+		PlayerLib.put(new Player(name,pass),name);
+		return false;
+		
+	}
+	
+	private boolean SameNameHasExist() {
+		
+		
+		
+	}
+	
+	
+	
 }
